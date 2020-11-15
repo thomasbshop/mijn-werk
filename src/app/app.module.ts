a4475b4661d3da47f80cc8b32d603c28ebb9ad13
Thomas Juma <thomas.juma@outlook.com>
2020-11-15 23:57:03 +0300

    Significant changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,21 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+// import { NzFormControlComponent, NzFormItemComponent, NzFormModule } from 'ng-zorro-antd/form';
+import { PagesModule } from './pages/pages.module';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PagesModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule

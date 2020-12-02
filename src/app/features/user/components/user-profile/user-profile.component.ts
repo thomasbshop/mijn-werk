dd0b567da5450cf5b9d9e3521aa02d273fb548c1
Thomas Juma <thomas.juma@outlook.com>
2020-12-02 17:42:35 +0300

    Modify user profile.

--- src/app/features/user/components/user-profile/user-profile.component.ts
+++ src/app/features/user/components/user-profile/user-profile.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from 'src/app/core/auth/services/authentication.service';
+import { User } from '../../interfaces/user';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit, OnDestroy {
+  public currentUser: any = null;
+  public user?: User;
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthenticationService,
+    // private loadingService: LoadingService,
+    private route: ActivatedRoute,
+    private db: AngularFirestore
+  ) {
+    // this.loadingService.isLoading.next(true);
   }
 
+  ngOnInit() {
+    this.subscriptions.push(
+      this.auth.currentUser.subscribe( user => {
+        this.currentUser = user;
+        console.log(this.currentUser);
+        // this.loadingService.isLoading.next(false);
+      })
+    );
+
+    this.subscriptions.push(
+      this.route.paramMap.subscribe( params => {
+        const userId = params.get('userId');
+        const userRef: AngularFirestoreDocument<User> = this.db.doc(`users/${userId}`);
+        userRef.valueChanges().subscribe(user => this.user = user);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
--- src/app/features/user/user.component.ts
+++ src/app/features/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from 'src/app/core/auth/services/authentication.service';
+import { ChannelService } from '../chat/services/channel.service';
+import { User } from './interfaces/user';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-  channels: Array<any> = ['#1', '#2', '#3']
-  constructor() { }
+  private subscriptions: Subscription[] = [];
+  public user?: User;
+  public currentUser: any = null;
+  public channel?: any;
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthenticationService,
+    public channelService: ChannelService
+  ) {
+    this.subscriptions.push(
+      this.auth.currentUser.subscribe( user => {
+        this.currentUser = user;
+        console.log(this.currentUser);
+        // this.loadingService.isLoading.next(false);
+      })
+    );
+
+    // this.subscriptions.push(
+    //   this.channelService.selectedChannel.subscribe(channel => {
+    //     this.channel = channel;
+    //   })
+    // );
+   }
+
+  ngOnInit() {
   }
 
 }
